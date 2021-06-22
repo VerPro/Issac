a4f46b444cb1e3016acb44664dbeed33af122a95
Veronika Prokůpková <prokupkova.verca@gmail.com>
2021-06-22 21:44:46 +0200

    connecting functions

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 'use strict';
-//Nechat vyskočit varovné okénko, pokud user nezadává number
 const buttonGuessElm = document.querySelector('#buttonGuess');
 const resultElm = document.querySelector('.result');
 const buttonAgain = document.querySelector('#again');
 
 let numberIssac = Math.round(Math.random() * 100);
-let numberUser = document.querySelector('#inputGuessNumber');
+let numberUserElm = document.querySelector('#inputGuessNumber');
 
-buttonAgainFunction = () => {
+
+const buttonAgainFunction = () => {
   const aElm = document.createElement('a');
   aElm.textContent = 'Hraj se mnou znovu';
   aElm.classList.add('buttonAgain');
   aElm.setAttribute('href', 'index.html');
-  aElm.appendChild(buttonAgain);
+  buttonAgain.appendChild(aElm);
 };
 
-const guessNumber = (numberUser) => {
+
+const guessNumber = () => {
+  const numberUser= Number(numberUserElm.value)
   if (numberIssac === numberUser) {
-    resultElm.textContent = `Ano, myslím na číslo ${numberIssac}!`;
-  } else {
+    resultElm.textContent= `Ano, myslím na číslo  ${numberIssac}!`;
+  } else  {
     resultElm.textContent = `Ne, ${numberUser} není číslo, na které myslím. Myslím na číslo ${numberIssac}.`;
-    buttonAgainFunction();
-    numberUser.setAttribute('value', '');
-  }
+  } 
+
+  buttonAgainFunction();
 };
 
 buttonGuessElm.addEventListener('click', guessNumber);
