2762f9c46bbbe3546da13c668ae0925c83bf0a3c
Veronika Prokůpková <prokupkova.verca@gmail.com>
2021-06-22 21:59:37 +0200

    added atribute disabled on submit button

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 🤖 Issac je jednoduchý projektík, jehož základní funkci jsem napsala na začátku své zkušenosti s Javascriptem. Pojmenovala jsem ho po svém oblíbeném autoru sci-fi Issacu Asimovovi. Issac zvládne hrát hru na hádání čísel. Napsaný je v čistém Javascriptu a později bylo přidáno pár vylepšení. V současné chvíli bych ho určitě dělala jinak, ale z úcty ke svým začátkům ho nechávám tak, jak je.
 
 ---
+
 :gb: :us:
 
 🤖 Issac is a simple project whose core function I wrote at the begining of learning Javascript. I named it after my favorite sci-fi writer Issac Asimov. This Issac only know a game of guessing numbers. It is written in vanilla Javascript and I added some improvements afterwards. I could write it differently now but in honor of my begginings I let it as it was.
--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const buttonAgain = document.querySelector('#again');
 let numberIssac = Math.round(Math.random() * 100);
 let numberUserElm = document.querySelector('#inputGuessNumber');
 
-
 const buttonAgainFunction = () => {
   const aElm = document.createElement('a');
   aElm.textContent = 'Hraj se mnou znovu';
@@ -15,16 +14,16 @@ const buttonAgainFunction = () => {
   buttonAgain.appendChild(aElm);
 };
 
-
 const guessNumber = () => {
-  const numberUser= Number(numberUserElm.value)
+  const numberUser = Number(numberUserElm.value);
   if (numberIssac === numberUser) {
-    resultElm.textContent= `Ano, myslím na číslo  ${numberIssac}!`;
-  } else  {
+    resultElm.textContent = `Ano, myslím na číslo  ${numberIssac}!`;
+  } else {
     resultElm.textContent = `Ne, ${numberUser} není číslo, na které myslím. Myslím na číslo ${numberIssac}.`;
-  } 
+  }
 
   buttonAgainFunction();
+  buttonGuessElm.setAttribute('disabled', 'true');
 };
 
 buttonGuessElm.addEventListener('click', guessNumber);
